66b1a81f6a9317c4441914c88cf69dc08f87a26d
Shubhranshu <shubhranshu.lumia@gmail.com>
2020-10-11 22:49:03 +0530

    template

---
+++ template.cpp
@@ -0,0 +1,16 @@
+#include <cstdio>
+#include <iostream>
+#include <cmath>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <unordered_map>
+#include <map>
+
+#define ll long long int
+#define pii pair<int,int>
+#define FOR(i, st, ed) for(int i = int(st); i < int(ed); i++)
+#define divide(l, h, idx) int mid = (l + h)>>1, lc = idx<<1, rc = lc|1;
+#define MAX 100007
+using namespace std;
